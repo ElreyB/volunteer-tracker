79895d3cdbdc6469ba76a60ce39a52359a0b17be
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-22 09:39:17 -0700

    add #== to Project class that ckecks for equality based on the attributes of a volunteer

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -6,4 +6,10 @@ class Volunteer
     @name = args[:name]
     @project_id = args.fetch(:id){ nil }
   end
+
+  def ==(other_volunteer)
+    self.id == other_volunteer.id &&
+    self.name == other_volunteer.name &&
+    self.project_id == other_volunteer.project_id
+  end
 end
